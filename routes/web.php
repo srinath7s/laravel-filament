f7472ce61bc38298aa60afa3a314001d9c9ae5e7
srinath7s <srinath.s@arkinfotec.com>
2024-10-30 00:42:22 +0530

    Revert "open AI updated"
    This reverts commit 32d19f11c01c0a9090402576c194c56b6a950e80.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,7 @@
 <?php
 
-use App\Filament\Resources\ProductResource\Pages\ListProducts;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('products', ListProducts::class);
